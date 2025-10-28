c696236740267b411bf0749dadce13086d3b1f9f
Manuel <manuel.pavonbuendia@educa.madrid.org>
2025-10-28 17:30:10 +0100

    commit EjecutarBotes.java

--- src/main/java/es/etg/psp/titanic/mm/Botes/EjecutarBote.java
+++ src/main/java/es/etg/psp/titanic/mm/Botes/EjecutarBote.java
@@ -1,7 +1,15 @@
 package es.etg.psp.titanic.mm.Botes;
 
-
+import java.util.Map;
 
 public class EjecutarBote {
+    public static void main(String[] args) throws InterruptedException {
+        String id = args[0];
+        Bote bote = new Bote(id);
+        Map<String,Integer> pasajeros = bote.generarPasajeros();
 
+        StringBuilder sb = new StringBuilder();
+        pasajeros.forEach((k,v) -> sb.append(k).append("=").append(v).append(","));
+        System.out.println(sb.toString().replaceAll(",$",""));
+    }
 }
