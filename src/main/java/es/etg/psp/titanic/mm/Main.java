2763e1bfa2d67500801c59f292f295337cbbd240
Manuel <manuel.pavonbuendia@educa.madrid.org>
2025-10-29 10:28:49 +0100

    commit con Main.java hecho

--- src/main/java/es/etg/psp/titanic/mm/Main.java
+++ src/main/java/es/etg/psp/titanic/mm/Main.java
@@ -1,7 +1,10 @@
 package es.etg.psp.titanic.mm;
 
+import es.etg.psp.titanic.mm.ServicioEmergencia.ServicioEmergencia;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello titanic!");
+    public static void main(String[] args) throws Exception {
+        ServicioEmergencia servicio = new ServicioEmergencia();
+        servicio.iniciarSimulacion();
     }
 }
